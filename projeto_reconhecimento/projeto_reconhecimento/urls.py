b1041f10f6ad33d15e02f31f780fb62104ddb048
bvasconcelos710 <bvasconcelos710@gmail.com>
2025-05-26 22:11:04 -0300

    ajustando estilização

--- projeto_reconhecimento/projeto_reconhecimento/urls.py
+++ projeto_reconhecimento/projeto_reconhecimento/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_usuarios.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
